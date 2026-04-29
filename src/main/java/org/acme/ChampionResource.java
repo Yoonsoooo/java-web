e4464fa974aba30bbf946c544b602cf4d1590054
Yoonsooworld <chldbstn002@naver.com>
2026-04-29 14:32:37 +0900

    9주차 실습 완료

---
+++ src/main/java/org/acme/ChampionResource.java
@@ -0,0 +1,22 @@
+package org.acme;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/champions")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ChampionResource {
+    @GET
+    public List<Champion> list() {
+        return Champion.listAll();
+    }
+
+    @POST
+    @Transactional
+    public void add(Champion champion) {
+        champion.persist();
+    }
+}
